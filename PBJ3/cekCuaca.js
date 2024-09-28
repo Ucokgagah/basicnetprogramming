ef4c4c79683512ac050a4f27f7eb7222d524d701
Ucokgagah <rudikurniaalamin65@gmail.com>
2024-09-29 00:14:37 +0700

    deskripsi cuaca

--- PBJ3/cekCuaca.js
+++ PBJ3/cekCuaca.js
@@ -1,9 +1,11 @@
 const request = require('postman-request')
-const urlCuaca =
-'http://api.weatherstack.com/current?access_key=683736ed07ca9cc5cd6a27303770f5b7&query=-0.8969903936252707, 100.35076508055'
+const urlCuaca = 'http://api.weatherstack.com/current?access_key=683736ed07ca9cc5cd6a27303770f5b7&query=-0.8969903936252707,100.35076508055&units=m'
+
 request({ url: urlCuaca, json: true }, (error, response) => {
-console.log('Saat ini suhu diluar mencapai ' +
-response.body.current.temperature +
-' derajat celcius. Kemungkinan terjadinya hujan adalah' + response.body.current.precip
-+ '%')
+    console.log('Saat ini suhu diluar mencapai ' +
+        response.body.current.temperature +
+        ' derajat celcius. Kemungkinan terjadinya hujan adalah ' + 
+        response.body.current.precip +
+        '%. Kondisi cuaca: ' + response.body.current.weather_descriptions[0])
 })
+
