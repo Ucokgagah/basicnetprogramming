dfd3aaef38cb7177cd2c06e07934023c6a675c7f
Ucokgagah <rudikurniaalamin65@gmail.com>
2024-10-10 11:40:35 +0700

    menambahkan

--- PBJ5/web-server/src/app.js
+++ PBJ5/web-server/src/app.js
@@ -1,33 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
-// Halaman utama dengan format HTML
-app.get('', (req, res) => {
-    res.send('<h1>Selamat datang di halaman utama</h1>')
-})
+const app = express();
 
-// Halaman bantuan dengan format HTML
+// Mengatur path untuk direktori views dan public sebelum digunakan
+const direktoriPublic = path.join(__dirname, '../public');
+const direktoriviews = path.join(__dirname, '../templates'); 
+
+// Setup handlebars engine dan lokasi folder views
+app.set('view engine', 'hbs'); // Perbaikan di sini
+app.set('views', direktoriviews);
+
+app.set('views', direktoriviews);
+
+// Mengatur direktori public sebagai direktori untuk file statis
+app.use(express.static(direktoriPublic));
+
+// Halaman utama menggunakan Handlebars
+app.get('/', (req, res) => {
+    res.render('index', {
+        judul: 'Aplikasi Cek Cuaca',
+        nama: 'Rudi Kurnia Al A'
+    });
+});
+
+// Halaman tentang
+app.get('/tentang', (req, res) => {
+    res.render('tentang', {
+        judul: 'Tentang Saya',
+        nama: 'Rudi Kurnia Al A'
+    });
+});
+
+// Halaman bantuan
 app.get('/bantuan', (req, res) => {
-    res.send('<h1>Ini halaman bantuan</h1>')
-})
+    res.render('bantuan', {
+        judul: 'Halaman Bantuan',
+        teksBantuan: 'Ini adalah teks bantuan',
+        nama: 'Rudi Kurnia Al A'
+    });
+});
 
 // Halaman infoCuaca dengan format JSON
 app.get('/infoCuaca', (req, res) => {
     res.send([{
         prediksiCuaca: 'cuaca berpotensi hujan',
         lokasi: 'Padang'
-    }])
-})
-
-// Halaman tentang dengan format JSON
-app.get('/tentang', (req, res) => {
-    res.send([{
-        nama: 'Rudi Kurnia Al A',
-        pekerjaan: 'Mahasiswa'
-    }])
-})
+    }]);
+});
 
 // Server berjalan di port 4000
 app.listen(4000, () => {
-    console.log('Server berjalan pada port 4000.')
-})
+    console.log('Server berjalan pada port 4000.');
+});
